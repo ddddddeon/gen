eb8e90d2455decffe35380dfa69610f98b823d4b
ddddddeon <chris.deon@unity3d.com>
2023-05-26 22:23:09 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use gen::project::{Lang, Project, ProjectKind};
 use std::str::FromStr;
 
 #[derive(Parser)]
@@ -13,19 +14,15 @@ struct Args {
     domain: Option<String>,
 }
 
-use gen::project::{Lang, Project, ProjectKind};
-
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let name = Box::leak(args.name.into_boxed_str());
-
     let kind = match args.kind {
         Some(kind) => ProjectKind::from_str(&kind)?,
         None => ProjectKind::Executable,
     };
 
     let lang = Lang::from_str(&args.lang)?;
-
     if lang == Lang::Java && args.domain.is_none() {
         println!("Java project requires domain name! Use --domain option.");
         std::process::exit(1);
@@ -33,5 +30,6 @@ fn main() -> anyhow::Result<()> {
 
     let project = Project::new(name, lang, kind, args.domain);
     project.generate()?;
+
     Ok(())
 }
